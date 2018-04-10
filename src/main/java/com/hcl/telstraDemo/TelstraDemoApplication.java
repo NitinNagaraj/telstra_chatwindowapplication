b3eb612312fdf62c829db6431d41d9d561b3098b
NitinNagaraj <38080703+NitinNagaraj@users.noreply.github.com>
2018-04-10 19:07:07 +0530

    Add files via upload

---
+++ src/main/java/com/hcl/telstraDemo/TelstraDemoApplication.java
@@ -0,0 +1,12 @@
+package com.hcl.telstraDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelstraDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelstraDemoApplication.class, args);
+	}
+}
